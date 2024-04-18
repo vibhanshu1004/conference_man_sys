5362d5c6365cf83e2a791f8ca30ae953f8c5fb08
vibhanshu1004 <166505483+vibhanshu1004@users.noreply.github.com>
2024-04-18 14:19:41 +0530

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from myapp.models import ConferenceModel,TalkModel,MemberModel
-
-admin.site.register(ConferenceModel)
-admin.site.register(TalkModel)
-admin.site.register(MemberModel)
-# Register your models here.
